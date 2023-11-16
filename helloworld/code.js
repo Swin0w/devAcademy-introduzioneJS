ab6075a0ab08dc3063ac99ceba9bebb5e6db1a4c
Swin0w <marzolla.games@gmail.com>
2023-11-16 23:36:54 +0100

    File esterno

---
+++ helloworld/code.js
@@ -0,0 +1,8 @@
+        // Commento su una riga: Stampa a video Hello World!
+        document.write("File: Hello world!!");
+        console.log('File: Hello world');
+        /* Commento su più righe:
+        Stampa a 
+        video 
+        Hello World!
+        */
